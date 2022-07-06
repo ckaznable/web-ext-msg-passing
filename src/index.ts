24c4011a69e63a2d82c5b7434a28451b5f31913e
CK Aznable <kami25565@gmail.com>
2022-07-06 17:30:07 +0800

    change index.ts exports

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,4 @@
-export  {
-  listen,
-  listenGroup,
-  listenNamespaceGroup,
-  listenNamespace,
-  listenMiddleware,
-  listenNamespaceMiddleware,
-  removeListener,
-  removeNamespaceListener,
-  clear,
-  clearNamespace,
-  clearAll,
-  installListener,
-  uninstallListener,
-} from "./listener"
-
-export {
-  UnionSender,
-  Sender,
-  send,
-  sendWithResponse,
-  sendToContent,
-} from "./messenger"
-
-export {
-  DEFAULT_NAMESPACE,
-} from "./static"
-
-export {
-  ReceiverReply,
-  ReceiverHandle,
-  MessageHandle,
-  MessageHandleTemplate,
-  UnionMessageHandleTemplate,
-  MessageHandleParameter,
-  MessageHandleReplyData,
-  MiddlewareData,
-  PassingData,
-} from "./types"
+export * from "./listener"
+export * from "./messenger"
+export * from "./static"
+export * from "./types"
